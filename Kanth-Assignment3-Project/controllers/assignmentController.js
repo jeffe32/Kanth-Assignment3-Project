77dce9fd355a5d12dfec9c2346b7b3230d8b6040
jeffe32 <rishikanth.manotheepan@ontariotechu.net>
2025-11-23 20:18:25 -0800

    second commit

--- Kanth-Assignment3-Project/controllers/assignmentController.js
+++ Kanth-Assignment3-Project/controllers/assignmentController.js
@@ -1,43 +1,41 @@
-const Assignment = require('./models/assignment');
+const Assignment = require('../models/assignment');
 
-// List all assignments
+// READ
 exports.list = async (req, res) => {
   const assignments = await Assignment.find().sort({ dueDate: 1 });
-
   res.render('assignments/list', {
     title: "All Assignments",
     assignments
   });
 };
 
-// Show form to add
+// ADD FORM
 exports.addForm = (req, res) => {
   res.render('assignments/add', { title: "Add Assignment" });
 };
 
-// Add new assignment
+// ADD
 exports.add = async (req, res) => {
   await Assignment.create(req.body);
   res.redirect('/assignments');
 };
 
-// Show edit form
+// EDIT FORM
 exports.editForm = async (req, res) => {
   const assignment = await Assignment.findById(req.params.id);
-
   res.render('assignments/edit', {
     title: "Edit Assignment",
     assignment
   });
 };
 
-// Update assignment
+// EDIT
 exports.edit = async (req, res) => {
   await Assignment.findByIdAndUpdate(req.params.id, req.body);
   res.redirect('/assignments');
 };
 
-// Delete assignment
+// DELETE
 exports.delete = async (req, res) => {
   await Assignment.findByIdAndDelete(req.params.id);
   res.redirect('/assignments');
